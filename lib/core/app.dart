794ac9d7314030db9586ceebea2c81bb83caae5c
vitu <vitorcsharp@hotmail.com>
2023-07-06 21:52:41 -0300

    add new routes

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,9 +1,12 @@
 import 'package:finance/core/shared/routes/named_routes.dart';
-import 'package:finance/view/home/home_page.dart';
+import 'package:finance/view/home/home_page_view.dart';
 import 'package:finance/view/onboarding/onboarding_page.dart';
+import 'package:finance/view/profile/profile_page.dart';
 import 'package:finance/view/sign_in/sign_in_page.dart';
 import 'package:finance/view/sign_up/sign_up_page.dart';
 import 'package:finance/view/splash/splash_page.dart';
+import 'package:finance/view/stats/stats_page.dart';
+import 'package:finance/view/wallet/wallet_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -18,7 +21,10 @@ class App extends StatelessWidget {
         NamedRoute.splash: (context) => const SplashPage(),
         NamedRoute.signUp: (context) => const SignUpPage(),
         NamedRoute.signIn: (context) => const SignInPage(),
-        NamedRoute.homePage: (context) => const HomePage(),
+        NamedRoute.homePage: (context) => const HomePageView(),      
+        NamedRoute.stats: (context) => const StatsPage(),
+        NamedRoute.wallet: (context) => const WalletPage(),
+        NamedRoute.profile: (context) => const ProfilePage(),
       },
     );
   }
