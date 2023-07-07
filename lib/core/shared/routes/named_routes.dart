e3bc0aa27f1bb2d304783b9202d3f66886f35c44
vitu <vitorcsharp@hotmail.com>
2023-07-06 21:53:00 -0300

    add new named routes

--- lib/core/shared/routes/named_routes.dart
+++ lib/core/shared/routes/named_routes.dart
@@ -7,4 +7,7 @@ class NamedRoute {
   static const String signUp = "/sign_up";
   static const String signIn = "/sign_in";
   static const String homePage = "/home_page";
+  static const String stats = "/stats_page";
+  static const String wallet = "/wallet_page";
+  static const String profile = "/profile_page";
 }
