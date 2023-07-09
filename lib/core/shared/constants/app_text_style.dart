ba5c3a135a6946508b8eb59b1c81f845e896f065
vitu <vitorcsharp@hotmail.com>
2023-07-09 01:11:43 -0300

    add a new textStyles

--- lib/core/shared/constants/app_text_style.dart
+++ lib/core/shared/constants/app_text_style.dart
@@ -9,9 +9,20 @@ class AppTextStyle {
 
   static const TextStyle mediumText =
       TextStyle(fontSize: 28, fontWeight: FontWeight.w700, fontFamily: "Inter");
+  static const TextStyle mediumText16w600 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+  );
 
   static const TextStyle smallText =
       TextStyle(fontSize: 14, fontWeight: FontWeight.w500, fontFamily: "Inter");
+static const TextStyle smallText13 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 13.0,
+    fontWeight: FontWeight.w400,
+  );
+
 
   static const TextStyle buttonText =
       TextStyle(fontSize: 18, fontWeight: FontWeight.w600, fontFamily: "Inter");
@@ -20,9 +31,36 @@ class AppTextStyle {
       TextStyle(fontSize: 14, fontWeight: FontWeight.w500, fontFamily: "Inter");
   static const TextStyle inputHintText =
       TextStyle(fontSize: 12, fontWeight: FontWeight.w400, fontFamily: "Inter");
-  static const TextStyle inputHelperText =
-      TextStyle(fontSize: 12, fontWeight: FontWeight.w400, fontFamily: "Inter", color: AppColors.lightGreenTwo);
+  static const TextStyle inputHelperText = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Inter",
+      color: AppColors.lightGreenTwo);
 
-  static const TextStyle errorTextTryAgain =
-      TextStyle(fontSize: 16, fontWeight: FontWeight.w400, fontFamily: "Inter", color: AppColors.lightGreenTwo);
+  static const TextStyle errorTextTryAgain = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Inter",
+      color: AppColors.lightGreenTwo);
+
+  static const TextStyle mediumText30 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 30.0,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle mediumText16w500 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+    static const TextStyle mediumText20 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle mediumText18 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600,
+  );
 }
