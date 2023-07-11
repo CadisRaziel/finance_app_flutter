6dec6494a909b0ba9663b270a1519748f807c786
vitu <vitorcsharp@hotmail.com>
2023-07-10 21:20:40 -0300

    add a color parameter

--- lib/core/shared/components/circular_progress_indicator/custom_circular_progress_indicator.dart
+++ lib/core/shared/components/circular_progress_indicator/custom_circular_progress_indicator.dart
@@ -2,14 +2,15 @@ import 'package:finance/core/shared/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomCircularProgressIndicator extends StatelessWidget {
+  final Color? color;
 
-  const CustomCircularProgressIndicator({ super.key });
+  const CustomCircularProgressIndicator({ super.key, this.color });
 
    @override
    Widget build(BuildContext context) {
-       return const Center(
+       return Center(
          child:  CircularProgressIndicator(
-          color: AppColors.white,
+          color: color ?? AppColors.white,
          ),
        );
   }
