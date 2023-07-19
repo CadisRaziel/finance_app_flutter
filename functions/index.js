2af86674076114bee74e44d04c855de54a1684d1
vitu <vitorcsharp@hotmail.com>
2023-07-18 22:56:58 -0300

    making a small adjustment to the function by gitgnore

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,13 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const request = require("graphql-request");
 
-// const client = new request.GraphQLClient('https://concrete-leopard-63.hasura.app/v1/graphql', {
+
+// const client = new request.GraphQLClient('xxxxxxxx', {
 //     headers: {
 //         "content-type": "application/json",
-//         "x-hasura-admin-secret": "QXzvjsBmFcSzn7NwSujH8fMFXzNf3jDWLN7sT9Fgj0EkCpY22eu1ja9p0E3EaCac"
+//         "x-hasura-admin-secret": "xxxxxxxx"
 //     }
 // })
-const client = new request.GraphQLClient('xxxxxxxx', {
-    headers: {
-        "content-type": "application/json",
-        "x-hasura-admin-secret": "xxxxxxxx"
-    }
-})
 
 admin.initializeApp(functions.config().firebase);
 
