cf422af347e6bedf6c8072c0cfb99c046abb18c1
vitu <vitorcsharp@hotmail.com>
2023-07-19 21:57:49 -0300

    initializing the grapqhl service in our signup controller, because when the user creates the account it will now pass through the call of the grapqhl api

--- lib/view/sign_up/sign_up_controller.dart
+++ lib/view/sign_up/sign_up_controller.dart
@@ -1,13 +1,23 @@
-import 'package:finance/services/auth_service.dart';
-import 'package:finance/services/secure_storage.dart';
-import 'package:finance/view/sign_up/sign_up_state.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
+import 'package:finance/services/auth_service.dart';
+import 'package:finance/services/graphql_service.dart';
+import 'package:finance/services/secure_storage.dart';
+import 'package:finance/view/sign_up/sign_up_state.dart';
+
 class SignUpController extends ChangeNotifier {
-  final AuthService _authService; //Repare que eu injeto a interface
-   final  SecureStorage _secureStorage;
-  SignUpController(this._authService, this._secureStorage);
+  final AuthService authService; //Repare que eu injeto a interface
+  final SecureStorage secureStorage;
+  final GraphqlService graphqlService;
+
+  SignUpController({
+    required this.authService,
+    required this.secureStorage,
+    required this.graphqlService,
+  });
+
   var logger = Logger();
 
   SignUpState _state = SignUpInitialState();
@@ -21,15 +31,10 @@ class SignUpController extends ChangeNotifier {
       {required String name,
       required String email,
       required String password}) async {
-
-
-   
-
-
     _changeState(SignUpLoadingState());
     try {
       //criando usuario
-      final user = await _authService.signUp(
+      final user = await authService.signUp(
         name: name,
         email: email,
         password: password,
@@ -37,10 +42,14 @@ class SignUpController extends ChangeNotifier {
 
       //salvando usuario no secureStorage
       if (user.id != null) {
-        await _secureStorage.write(
+        await secureStorage.write(
           key: "CURRENT_USER",
           value: user.toJson(),
         );
+
+        //chamando a api do graphql após ter a certeza que ja tem um usuario
+        await graphqlService.init();
+      
         _changeState(SignUpSucessState());
       } else {
         throw Exception();
