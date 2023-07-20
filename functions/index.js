00d75e9badc48a0ddead8c3e7dd2579e965fc88c
vitu <vitorcsharp@hotmail.com>
2023-07-19 21:53:57 -0300

    adjusting cloud functions calls

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,12 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const request = require("graphql-request");
 
+const client = new request.GraphQLClient('https://concrete-leopard-63.hasura.app/v1/graphql', {
+    headers: {
+        "content-type": "application/json",
+        "x-hasura-admin-secret": "QXzvjsBmFcSzn7NwSujH8fMFXzNf3jDWLN7sT9Fgj0EkCpY22eu1ja9p0E3EaCac"
+    }
+})
 
 // const client = new request.GraphQLClient('xxxxxxxx', {
 //     headers: {
